5d5395a0de0f5ebb3a7ea9ff8349a85e7fe97611
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-04-01 23:20:57 +0300

    Type annotation added.

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -27,6 +27,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ Function that displays paginated pages. """
         assert type(page) is int
         assert type(page_size) is int
         assert page > 0
