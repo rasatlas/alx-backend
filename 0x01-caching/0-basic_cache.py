f2efa1cdd64de088373f169853e790cf6165f272
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-04-02 18:09:34 +0300

    Cache implementation using basic dictionary.

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Module defining a basic caching startegy using dictionary. """
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Class implementing basic caching using dictionary. """
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
