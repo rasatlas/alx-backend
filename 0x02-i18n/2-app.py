2280631f1d9dc981ff44dc9ce84b8418217888d3
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-04-10 02:21:41 +0300

    Refactor

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -20,7 +20,7 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale() -> str:
-    """Method to determine the bet match for supportd languages."""
+    """Method to determine the best match for supported languages."""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Parametrize templates. """
-from flask_babel import Babel, get_locale
-from flask import Flask, render_template, request, g
+from flask_babel import Babel
+from flask import Flask, render_template, request
 
 
 class Config:
@@ -15,15 +15,11 @@ class Config:
 app = Flask(__name__)
 babel = Babel(app)
 app.config.from_object(Config)
-babel.init_app(app, locale_selector=get_locale)
 
 
-# @babel.localeselector
+@babel.localeselector
 def get_locale() -> str:
     """Method to determine the best match for supported languages."""
-    user = getattr(g, "user", None)
-    if user is not None:
-        return user.locale
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
