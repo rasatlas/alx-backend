37fa2c08a016f313a16b54b9ae2038cc35a4d12f
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-04-13 17:27:12 +0300

    Basic Babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -14,8 +14,8 @@ class Config:
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-babel = Babel(app)
 app.config.from_object(Config)
+babel = Babel(app)
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
